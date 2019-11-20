c8098e657ce6b3d93cd8114db1adc55814716e66
James Bremner <james@ravenspoint.com>
2019-11-20 13:48:18 -0500

    Drawlist

---
+++ src/pack2.cpp
@@ -0,0 +1,264 @@
+#include <iostream>
+
+#include "pack2.h"
+
+#define INSTRUMENT 1
+
+using namespace std;
+
+namespace pack2
+{
+
+void Add( cPackEngine& e, bin_t bin, item_t item )
+{
+#ifdef INSTRUMENT
+    cout << "Adding item " << item->progID() << " to bin " << bin->progID() << "\n";
+#endif // INSTRUMENT
+
+    // if adding first item to root bin
+    // and there is an endless supply available
+    // make a new copy ready for next time
+    if( ! bin->isSub() )
+    {
+        if( bin->canCopy() )
+        {
+            e.add( bin_t( new cBin( bin ) ));
+        }
+    }
+
+    // add item to bin contents
+    bin->add( item );
+
+    // locate item relative to parent bin
+    item->locate( bin->locX(), bin->locY() );
+
+    // create new bin from remaining space to right of inserted item
+    bin_t newbin = bin_t( new cBin( "", bin->sizX() - item->sizX(), item->sizY() ));
+    newbin->locate( bin->locX() + item->sizX(), bin->locY() );
+    if( bin->isSub() )
+        newbin->parent( bin->parent() );
+    else
+        newbin->parent( bin );
+    e.add( newbin );
+
+    // create new bin below and to right of inserted item
+    newbin = bin_t( new cBin( "", bin->sizX(), bin->sizY() - item->sizY() ));
+    newbin->locate( bin->locX(), bin->locY() + item->sizY() );
+    if( bin->isSub() )
+        newbin->parent( bin->parent() );
+    else
+        newbin->parent( bin );
+    e.add( newbin );
+}
+
+void Pack( cPackEngine& e )
+{
+    // try packing larger items first
+    // so the smaller may fit into odd remaining spaces
+    SortItemsIntoDecreasingSize( e );
+
+    // loop until no more items can be packed
+    while( 1 )
+    {
+        // true if an item was successfully packed
+        bool itemPacked = false;
+
+        // try fitting into the smaller spaces first
+        SortBinsIntoIncreasingSize( e );
+
+        // loop over items
+        for( item_t item : e.items() )
+        {
+            if( item->isPacked() )
+                continue;
+
+            // item is waiting to be packed
+
+            // loop over bins
+            for( bin_t bin : e.bins() )
+            {
+                if( bin->isPacked() )
+                    continue;
+
+                // bin is empty
+
+                if( Fits( item, bin ) )
+                {
+                    // item fits into bin
+
+                    Add( e, bin, item );
+                    itemPacked = true;
+
+                    // packing the item invalidates iterators
+                    // so we must restart the loops
+                    break;
+                }
+            }
+            if( itemPacked )
+                break;
+        }
+
+        // give up if no item found that fit anywhere
+        if( ! itemPacked )
+        {
+#ifdef INSTRUMENT
+            cout << "no more items will fit\n";
+#endif // INSTRUMENT
+            break;
+        }
+    }
+
+    // remove any unused bins
+    // and sub bins created from remaining space when item was packed into bin
+    RemoveUnusedBins( e );
+}
+
+
+
+bool Fits( item_t item, bin_t bin )
+{
+#ifdef INSTRUMENT
+    std::cout << "Trying to fit item " << item->progID()
+              << " into bin " <<bin->userID() <<" " << bin->progID()
+              <<" " << bin->sizX() <<" "<< bin->sizY()<< "\n";
+#endif // INSTRUMENT
+
+    if ( item->sizX() <= bin->sizX() && item->sizY() <= bin->sizY() )
+        return true;
+    if( item->canSpin() )
+        item->spin();
+    return ( item->sizX() <= bin->sizX() && item->sizY() <= bin->sizY() );
+}
+
+void RemoveUnusedBins( cPackEngine& e )
+{
+    std::vector< bin_t >& bins = e.bins();
+    bins.erase(
+        remove_if(
+            bins.begin(),
+            bins.end(),
+            [ ] ( bin_t b )
+    {
+        if( b->isSub() )
+            return true;
+        return ( ! b->isUsed() );
+
+    } ),
+    bins.end() );
+}
+void SortItemsIntoDecreasingSize( cPackEngine& e )
+{
+    std::vector<item_t>& items = e.items();
+    sort( items.begin(), items.end(),
+          []( item_t a, item_t b )
+    {
+        return a->size() > b->size();
+    });
+}
+void SortBinsIntoIncreasingSize( cPackEngine& e )
+{
+    auto& bins = e.bins();
+    sort( bins.begin(), bins.end(),
+          []( bin_t a, bin_t b )
+    {
+        return a->size() < b->size();
+    });
+}
+int BinCount( cPackEngine& e)
+{
+    int count = 0;
+    for( bin_t bin : e.bins() )
+    {
+        if( bin->isUsed() )
+            count++;
+    }
+    return count;
+}
+std::string CSV( cPackEngine& e )
+{
+    std::stringstream ss;
+    ss << "bin_name,item_name,item_left,item_top,rotated\n";
+    for( bin_t bin : e.bins() )
+    {
+        for( item_t item : bin->contents() )
+        {
+            ss << bin->userID() << "," << item->userID()
+               << "," << item->locX() <<"," << item->locY() << ",";
+            if( item->isSpun() )
+                ss << "rotated";
+            else
+                ss << "fixed";
+            ss << "\n";
+        }
+    }
+    return ss.str();
+}
+
+std::string Unpacked( cPackEngine& e )
+{
+    std::stringstream ss;
+    for( item_t item : e.items() )
+    {
+        if( ! item->isPacked() )
+            ss << item->userID() << ",";
+    }
+    return ss.str();
+}
+std::string CutList( cPackEngine& e )
+{
+    std::stringstream ss;
+    return ss.str();
+}
+std::string DrawList( cPackEngine& e )
+{
+    std::stringstream ss;
+    for( bin_t bin : e.bins() )
+    {
+        ss << DrawList( bin );
+    }
+    return ss.str();
+}
+std::string DrawList( bin_t bin )
+{
+    std::stringstream ss;
+
+    ss << "S.bin,"<<bin->userID()<<"\n";
+    ss << "S.color( 0xFF0000 );\n";
+    ss << "S.rectangle( { "
+       << 0 <<", "
+       << 0 << ", "
+       << (int)( bin->sizX() )  << ", "
+       << (int)( bin->sizY() ) << " } );\n";
+    ss << "S.color( 0x00FF );\n";
+
+    for( item_t item : bin->contents() )
+    {
+        ss << DrawList( item );
+    }
+    return ss.str();
+}
+std::string DrawList( item_t item )
+{
+    const float scale = 1;
+    float left = item->locX() / scale;
+    float top  = item->locY() / scale;
+    float w = item->sizX() / scale;
+    float h = item->sizY() / scale;
+
+    std::stringstream ss;
+    ss << fixed /* << setprecision(1) */;
+    ss << "S.rectangle( { "
+       << left <<", "
+       << top << ", "
+       << w << ", "
+       << h << " } );\n";
+    ss << "S.text(\"" << item->userID() <<"\", {"
+       << left+5 <<", "
+       << top+5 <<", 50,30 } );\n";
+
+    return ss.str();
+}
+
+int cShape::myLastProgID = -1;
+
+}
--- src/pack2.h
+++ src/pack2.h
@@ -1,9 +1,15 @@
-#define INSTRUMENT 1
+#pragma once
+
+#include <string>
+#include <sstream>
+#include <vector>
+#include <memory>
+#include <algorithm>
 
 namespace pack2
 {
 class cBin;
-typedef shared_ptr< cBin > bin_t;
+typedef std::shared_ptr< cBin > bin_t;
 
 class cShape
 {
@@ -29,11 +35,11 @@ public:
     {
         return myY;
     }
-    virtual string userID() const
+    virtual std::string userID() const
     {
         return myUserID;
     }
-    void userID( const string& id )
+    void userID( const std::string& id )
     {
         myUserID = id;
     }
@@ -90,14 +96,14 @@ public:
     {
         return myX * myY;
     }
-    string text() const
+    std::string text() const
     {
-        stringstream ss;
+        std::stringstream ss;
         ss << myUserID <<" "<< myProgID <<" "<< myX <<" x " << myY;
         return ss.str();
     }
 private:
-    string myUserID;
+    std::string myUserID;
     int myX;
     int myY;
     bool myfSpun;               // true if item was rotated when packed
@@ -109,8 +115,6 @@ private:
     bool myfPacked;
 
 };
-
-int cShape::myLastProgID = -1;
 
 class cItem : public cShape
 {
@@ -124,7 +128,7 @@ public:
 private:
 
 };
-typedef shared_ptr< cItem > item_t;
+typedef std::shared_ptr< cItem > item_t;
 
 class cBin : public cShape
 {
@@ -143,7 +147,7 @@ public:
         , myCopyCount( old->myCopyCount+1)
         , myParent( NULL )
     {
-        userID( userID() + "_cpy" + to_string( myCopyCount ) );
+        userID( userID() + "_cpy" + std::to_string( myCopyCount ) );
     }
     void add( item_t item )
     {
@@ -171,7 +175,7 @@ public:
     {
         return (bool) myContent.size();
     }
-    vector< item_t >& contents()
+    std::vector< item_t >& contents()
     {
         return myContent;
     }
@@ -189,33 +193,19 @@ public:
     }
     void spinEnable( bool f = true )
     {
-        throw runtime_error("Bins cannot be rotated");
+        throw std::runtime_error("Bins cannot be rotated");
     }
 
 private:
     bool myfCopy;                    ///< true if endless supply available
     int myCopyCount;
-    vector< item_t > myContent;
+    std::vector< item_t > myContent;
     bin_t myParent;
 };
 
 
 /// true if item fits inside bin
-bool Fits( item_t item, bin_t bin )
-{
-#ifdef INSTRUMENT
-    cout << "Trying to fit item " << item->progID()
-         << " into bin " <<bin->userID() <<" " << bin->progID()
-         <<" " << bin->sizX() <<" "<< bin->sizY()<< "\n";
-#endif // INSTRUMENT
-
-    if ( item->sizX() <= bin->sizX() && item->sizY() <= bin->sizY() )
-        return true;
-    if( item->canSpin() )
-        item->spin();
-    return ( item->sizX() <= bin->sizX() && item->sizY() <= bin->sizY() );
-}
-
+bool Fits( item_t item, bin_t bin );
 
 class cPackEngine
 {
@@ -241,195 +231,50 @@ public:
         myItem.clear();
         myBin.clear();
     }
-    vector< bin_t > & bins()
+    std::vector< bin_t > & bins()
     {
         return myBin;
     }
-    vector< item_t > & items()
+    std::vector< item_t > & items()
     {
         return myItem;
     }
 private:
-    vector< item_t > myItem;
-    vector< bin_t > myBin;
+    std::vector< item_t > myItem;
+    std::vector< bin_t > myBin;
 };
 
 
 
 /// pack item into bin
-void Add( cPackEngine& e, bin_t bin, item_t item )
-{
-#ifdef INSTRUMENT
-    cout << "Adding item " << item->progID() << " to bin " << bin->progID() << "\n";
-#endif // INSTRUMENT
+void Add( cPackEngine& e, bin_t bin, item_t item );
 
-    // if adding first item to root bin
-    // and there is an endless supply available
-    // make a new copy ready for next time
-    if( ! bin->isSub() )
-    {
-        if( bin->canCopy() )
-        {
-            e.add( bin_t( new cBin( bin ) ));
-        }
-    }
-
-    // add item to bin contents
-    bin->add( item );
-
-    // locate item relative to parent bin
-    item->locate( bin->locX(), bin->locY() );
-
-    // create new bin from remaining space to right of inserted item
-    bin_t newbin = bin_t( new cBin( "", bin->sizX() - item->sizX(), item->sizY() ));
-    newbin->locate( bin->locX() + item->sizX(), bin->locY() );
-    if( bin->isSub() )
-        newbin->parent( bin->parent() );
-    else
-        newbin->parent( bin );
-    e.add( newbin );
-
-    // create new bin below and to right of inserted item
-    newbin = bin_t( new cBin( "", bin->sizX(), bin->sizY() - item->sizY() ));
-    newbin->locate( bin->locX(), bin->locY() + item->sizY() );
-    if( bin->isSub() )
-        newbin->parent( bin->parent() );
-    else
-        newbin->parent( bin );
-    e.add( newbin );
-}
 /// Remove unused and sub bins
-void RemoveUnusedBins( cPackEngine& e )
-{
-    vector< bin_t >& bins = e.bins();
-    bins.erase(
-        remove_if(
-            bins.begin(),
-            bins.end(),
-            [ ] ( bin_t b )
-    {
-        if( b->isSub() )
-            return true;
-        return ( ! b->isUsed() );
+void RemoveUnusedBins( cPackEngine& e );
 
-    } ),
-    bins.end() );
-}
 
-void SortItemsIntoDecreasingSize( cPackEngine& e )
-{
-    vector<item_t>& items = e.items();
-    sort( items.begin(), items.end(),
-          []( item_t a, item_t b )
-    {
-        return a->size() > b->size();
-    });
-}
-void SortBinsIntoIncreasingSize( cPackEngine& e )
-{
-    auto& bins = e.bins();
-    sort( bins.begin(), bins.end(),
-          []( bin_t a, bin_t b )
-    {
-        return a->size() < b->size();
-    });
-}
+void SortItemsIntoDecreasingSize( cPackEngine& e );
+
+void SortBinsIntoIncreasingSize( cPackEngine& e );
+
 
 /// Pack items into bins
-void Pack( cPackEngine& e )
-{
-    // try packing larger items first
-    // so the smaller may fit into odd remaining spaces
-    SortItemsIntoDecreasingSize( e );
-
-    // loop until no more items can be packed
-    while( 1 )
-    {
-        // true if an item was successfully packed
-        bool itemPacked = false;
-
-        // try fitting into the smaller spaces first
-        SortBinsIntoIncreasingSize( e );
-
-        // loop over items
-        for( item_t item : e.items() )
-        {
-            if( item->isPacked() )
-                continue;
-
-            // item is waiting to be packed
-
-            // loop over bins
-            for( bin_t bin : e.bins() )
-            {
-                if( bin->isPacked() )
-                    continue;
-
-                // bin is empty
-
-                if( Fits( item, bin ) )
-                {
-                    // item fits into bin
-
-                    Add( e, bin, item );
-                    itemPacked = true;
-
-                    // packing the item invalidates iterators
-                    // so we must restart the loops
-                    break;
-                }
-            }
-            if( itemPacked )
-                break;
-        }
-
-        // give up if no item found that fit anywhere
-        if( ! itemPacked )
-        {
-#ifdef INSTRUMENT
-            cout << "no more items will fit\n";
-#endif // INSTRUMENT
-            break;
-        }
-    }
-
-    // remove any unused bins
-    // and sub bins created from remaining space when item was packed into bin
-    RemoveUnusedBins( e );
-}
-
-
+void Pack( cPackEngine& e );
 
 /// Number of bins used
-int BinCount( cPackEngine& e)
-{
-    int count = 0;
-    for( bin_t bin : e.bins() )
-    {
-        if( bin->isUsed() )
-            count++;
-    }
-    return count;
-}
+int BinCount( cPackEngine& e);
+
 /// Packing result as a CSV string
-std::string CSV( cPackEngine& e )
-{
-    stringstream ss;
-    ss << "bin_name,item_name,item_left,item_top,rotated\n";
-    for( bin_t bin : e.bins() )
-    {
-        for( item_t item : bin->contents() )
-        {
-            ss << bin->userID() << "," << item->userID()
-               << "," << item->locX() <<"," << item->locY() << ",";
-            if( item->isSpun() )
-                ss << "rotated";
-            else
-                ss << "fixed";
-            ss << "\n";
-        }
-    }
-    return ss.str();
+std::string CSV( cPackEngine& e );
+
+/// CSV item ids that could not be fittted
+std::string Unpacked( cPackEngine& e );
+
+std::string CutList( cPackEngine& e );
+
+std::string DrawList( cPackEngine& e );
+std::string DrawList( bin_t bin );
+std::string DrawList( item_t item );
 }
-}
+
 
